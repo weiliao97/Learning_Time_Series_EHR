e30475da0bb2f723712ea55fe095f587306eba8e
Wei Liao <36976311+weiliaomit@users.noreply.github.com>
2023-03-28 15:46:39 -0400

    remove

--- loss_fn.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-import torch
-
-mse_loss = nn.MSELoss()
-def mse_maskloss(output, target, mask):
-    loss = [mse_loss(output[i][mask[i]==0], target[i][mask[i]==0]) \
-            for i in range(len(output))]
-    return torch.mean(torch.stack(loss))
