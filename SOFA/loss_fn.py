e264eb1d528921e85a19e1effdb18adeeedb2649
root <liaoweithu@gmail.com>
2023-04-09 01:22:48 +0000

    dir change

---
+++ SOFA/loss_fn.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+import torch
+
+mse_loss = nn.MSELoss()
+def mse_maskloss(output, target, mask):
+    """
+    
+    """
+    loss = [mse_loss(output[i][mask[i]==0], target[i][mask[i]==0]) \
+            for i in range(len(output))]
+    return torch.mean(torch.stack(loss))
